8c5b6a8c008dd6d5cf0e3e9f00b7823b1a6b83aa
Mantissagithub <mantissa6789@gmail.com>
2025-01-09 20:27:11 +0530

    removed build, log and install from and updated .gitignore

--- irc_autonomous_ws/build/autonomous_stack/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/pradheep/Documents/strawberry_cheese_cake/irc_autonomous_ws/install/autonomous_stack'
